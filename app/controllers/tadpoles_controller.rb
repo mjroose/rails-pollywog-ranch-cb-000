c1b527def3df773db329530bb294a3c3153c1153
Michael Roose <mjroose@gmail.com>
2018-11-26 21:27:58 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -4,6 +4,7 @@ class TadpolesController < ApplicationController
   def metamorphose
     @tadpole = Tadpole.find(params[:id])
     @frog = @tadpole.metamorphose
+    binding.pry
     @tadpole.destroy
     redirect_to frog_path(@frog)
   end
